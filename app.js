68f71c46f19e3a7777bfaa649d0d1e7d9f3eb35b
Greg Johle <greg.johle@gmail.com>
2020-12-28 14:22:08 -0500

    Finished

--- app.js
+++ app.js
@@ -1,43 +1,71 @@
 // This is the JavaScript for part one of this assignment
 
-function resultsTemplate(doggy) {
-    return `<img src="${doggy}" class="doge" alt="an adorable picture of a wonderful dog"`
-};
-
-function displayResults(responseJson) {
-    var indexDoggos = responseJson.message.length;
-    for (let i = 0; i < indexDoggos; i++) {
-        var picRaw = responseJson.array[i];
-        var picFormat = resultsTemplate(picRaw);
-        $('.results').append(picFormat);
-    };
-    $('.results').removeClass('.hidden');
-};
-
-// This function fetches the images from the Dog API
+// This function fetches the images from the Dog API, if no value is entered, the default value of 3 is used.
 function getDogImages(number) {
-    var doggos = `https://dog.ceo/api/breeds/image/random/${number}`
-    console.log(doggos)
-    fetch(doggos)
-        .then(response => response.json())
-        // This logs the reply json in the console for part one
-        .then(responseJson => console.log(responseJson))
-        // This moves the response to the display function for part two
-        .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('An error has occurred. Please try again later.'));
+    if (1 > number) {
+        fetch(`https://dog.ceo/api/breeds/image/random/3`)
+            .then(response => response.json())
+            .then(responseJson => displayResults(responseJson))
+            .catch(error => alert('An error has occurred. Please try again later.'));
+    } else {
+        var doggos = `https://dog.ceo/api/breeds/image/random/${number}`
+        console.log(doggos)
+        fetch(doggos)
+            .then(response => response.json())
+            .then(responseJson => displayResults(responseJson))
+            .catch(error => alert('An error has occurred. Please try again later.'));
+    };
 };
+
+// This returns a random dog of a specific breed
+function getDogBreed(breed) {
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random/1`)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('My apologies, but that breed was not found.'));
+};
+
+// This adds a list item for each result
+function displayResults(responseJson) {
+    console.log(responseJson);
+    let responseStatus = responseJson.status;
+    let imageUrls = responseJson.message;
+
+    if (responseStatus === "success") {
+        for (let i = 0; i < imageUrls.length; i++) {
+            $('#js-results').append(
+                `<li><img src="${responseJson.message[i]}" class="results-img"></li>`
+            );
+        };
+    } else {
+        alert('My apologies, but that breed was not found.');
+    };
+}
+
+
 
 // This function listens for the submit on part one and part two and passes the form value to the getDogImages function
 function watchForm() {
     $('.partOne').submit(function(event) {
         event.preventDefault();
-        var numDoggos = $('input[name="number"]').val();
-        getDogImages(numDoggos);
+        $('#js-results').empty();
+        $('#breedOfDogs').empty();
+        getDogImages($('#numberOfDogs').val());
+    });
+};
+
+function watchBreed() {
+    $('.breed').submit(function(event) {
+        event.preventDefault();
+        $('#js-results').empty();
+        $("#numberOfDogs").empty();
+        getDogBreed($('#breedOfDogs').val());
     });
 };
 
 // This function allows for the JavaScript to run after the page has loaded
 $(function() {
-    console.log('part one is ready');
+    console.log('Ready');
     watchForm();
+    watchBreed();
 });
